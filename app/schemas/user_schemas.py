23d4469dd2929d14f2c5074d2c969c49cc3aec9c
sathwik12345h <sadwikvaema@gmail.com>
2025-04-16 20:59:14 -0400

    profile image fixes

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,7 +1,7 @@
 from builtins import ValueError, any, bool, str
+from urllib.parse import urlparse
 from pydantic import BaseModel, EmailStr, Field, validator, root_validator
 from typing import Optional, List
-from datetime import datetime
 from enum import Enum
 import uuid
 import re
@@ -28,7 +28,7 @@ class UserBase(BaseModel):
     first_name: Optional[str] = Field(None, example="John")
     last_name: Optional[str] = Field(None, example="Doe")
     bio: Optional[str] = Field(None, example="Experienced software developer specializing in web applications.")
-    profile_picture_url: Optional[str] = Field(None, example="https://example.com/profiles/john.jpg")
+    profile_picture_url: Optional[str] = Field(None, description="The user's profile picture link. Should have a valid image file (e.g., .jpg, .jpeg, .png).", example="https://example.com/profiles/john.jpg")
     linkedin_profile_url: Optional[str] =Field(None, example="https://linkedin.com/in/johndoe")
     github_profile_url: Optional[str] = Field(None, example="https://github.com/johndoe")
 
@@ -42,6 +42,15 @@ class UserBase(BaseModel):
             if value.lower() in ["admin", "root", "superuser"]:
                 raise ValueError("This nickname is not allowed")
         return value
+    
+    @validator('profile_picture_url', pre=True, always=True)
+    def validate_profile_picture_url(cls, value):
+        if value is None:
+            return value  # If the URL is optional, allow None values
+        parsed_url = urlparse(value)
+        if not re.search(r"\.(jpg|jpeg|png)$", parsed_url.path):
+            raise ValueError("Should have a valid image file (e.g., .jpg, .jpeg, .png)")
+        return value
     class Config:
         from_attributes = True
 
@@ -50,18 +59,18 @@ class UserCreate(UserBase):
     password: str = Field(..., min_length=6, example="Secure*1234")
 
     @validator('password')
-    def validate_password(cls, v):
-        if len(v) < 6:
+    def validate_password(cls, value):
+        if len(value) < 6:
             raise ValueError("Password must be at least 6 characters long.")
-        if not re.search(r"[A-Z]", v):
+        if not re.search(r"[A-Z]", value):
             raise ValueError("Password must contain at least one uppercase letter.")
-        if not re.search(r"[a-z]", v):
+        if not re.search(r"[a-z]", value):
             raise ValueError("Password must contain at least one lowercase letter.")
-        if not re.search(r"\d", v):
+        if not re.search(r"\d", value):
             raise ValueError("Password must contain at least one digit.")
-        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", v):
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
             raise ValueError("Password must contain at least one special character.")
-        return v
+        return value
 
 class UserUpdate(UserBase):
     email: Optional[EmailStr] = Field(None, example="john.doe@example.com")
